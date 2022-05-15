83554fe94c78a692b34c6f2d7d7f6650b9a36896
ICS-ShanghaiTech <tracer@ics>
2022-05-15 12:44:02 +0800

    an 2020231013

--- projects/lab3/asm/task1.asm
+++ projects/lab3/asm/task1.asm
@@ -8,11 +8,11 @@ loop :  lw      x9,     0(x5)
         lw      x10,    0(x6)
         mul     x10,    x10,    x9
         lw      x11,    0(x7)
-        add     x11,    x11,    x9
+        add     x11,    x11,    x10
         sw      x11,    0(x8)
         addi    x5,     x5,     4
         addi    x6,     x6,     4
         addi    x7,     x7,     4
         addi    x8,     x8,     4
         addi    x30,    x30,    1
-        bne     x30,    x31,    -88
+        bne     x30,    x31,    loop
