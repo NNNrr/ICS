4ac48d34c2fb574632615d0b7b22d1ba87672ac3
ICS-ShanghaiTech <tracer@ics>
2022-05-15 13:23:14 +0800

    an 2020231013

--- projects/lab3/asm/task1.asm
+++ projects/lab3/asm/task1.asm
@@ -6,10 +6,10 @@
         addi    x8,     x4,     0   ; addr_D = D_baseaddr
         lw      x9,     0(x5)
         lw      x10,    0(x6)
-        mul     x10,    x10,    x9
-        lw      x11,    0(x7)
-        add     x11,    x11,    x10
-        sw      x11,    0(x8)
+        mul     x11,    x10,    x9
+        lw      x12,    0(x7)
+        add     x13,    x12,    x11
+        sw      x13,    0(x8)
         addi    x5,     x5,     4
         addi    x6,     x6,     4
         addi    x7,     x7,     4
