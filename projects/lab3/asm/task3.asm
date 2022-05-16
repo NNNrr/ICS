b5cbdd59e975275c2a0ea457c2cc87146bdde0d9
ICS-ShanghaiTech <tracer@ics>
2022-05-17 00:02:09 +0800

    an 2020231013

--- projects/lab3/asm/task3.asm
+++ projects/lab3/asm/task3.asm
@@ -2,14 +2,10 @@ addi    x5,     x1,     0   ; addr_A = A_baseaddr
 addi    x6,     x2,     0   ; addr_B = B_baseaddr
 addi    x7,     x3,     0   ; addr_C = C_baseaddr
 addi    x8,     x4,     0   ; addr_D = D_baseaddr
-
 addi    x9,     zero,   8
-addi    x10,    zero,   0
-
 vle32.v vx2,    x5,     1
 vle32.v vx3,    x6,     1
 vle32.v vx4,    x7,     1
-
 vmac.lw vx4
 vmac.en 0,      vx2,    vx3
 addi    x5,     x5,     32
