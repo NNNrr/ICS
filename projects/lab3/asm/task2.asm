c393e982d070a1886f0d41abc485c4451a6c29b9
ICS-ShanghaiTech <tracer@ics>
2022-05-16 18:48:33 +0800

    an 2020231013

--- projects/lab3/asm/task2.asm
+++ projects/lab3/asm/task2.asm
@@ -4,6 +4,8 @@ addi    x7,     x3,     0   ; addr_C = C_baseaddr
 addi    x8,     x4,     0   ; addr_D = D_baseaddr
 
 addi    x9,     x8,     256
+addi    x12,    zero,   8
+addi    x13,    zero,   0
 vle32.v vx2,    x5,     1           ; vx2 = mem[addr_A]
 vle32.v vx3,    x6,     1           ; vx3 = mem[addr_B]
 vle32.v vx5 ,   x7,     1
@@ -13,7 +15,15 @@ vmul.vv vx4,    vx3,    vx2,    0   ; vx4 = vx2 * vx3
 vse32.v vx4,    x9,     1           ; mem[addr_D] = vx1
 
 lw      x10,    0(x9)
-sw      x10,    0(x8)
+add     x11,    x11,    x10
+
+addi    x9,     x9,     4
+addi    x13,    x13,    1
+bne     x13,    x12,    -20    
+
+add     x11,    x11,    x7
+sw      x11,    0(x8)  
+
 
 
 
