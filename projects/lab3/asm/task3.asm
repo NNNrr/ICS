0a64080a9f50832ebe3e1a5a7945221b67aa5b08
ICS-ShanghaiTech <tracer@ics>
2022-05-16 23:28:25 +0800

    an 2020231013

---
+++ projects/lab3/asm/task3.asm
@@ -0,0 +1,43 @@
+addi    x5,     x1,     0   ; addr_A = A_baseaddr
+addi    x6,     x2,     0   ; addr_B = B_baseaddr
+addi    x7,     x3,     0   ; addr_C = C_baseaddr
+addi    x8,     x4,     0   ; addr_D = D_baseaddr
+
+addi    x9,     zero,   8
+addi    x10,    zero,   0
+
+vle32.v vx2,    x5,     1
+vle32.v vx3,    x6,     1
+vle32.v vx4,    x7,     1
+vmac.lw vx4
+vmac.en 0,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.en 1,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.en 2,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.en 3,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.en 4,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.en 5,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.en 6,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.en 7,      vx2,    vx3
+addi    x5,     x5,     32
+vle32.v vx2,    x5,     1
+vmac.sw vx1
+vse32.v vx1     x8,     1
+addi    x5,     x1,     0   
+addi    x7,     x7,     32
+addi    x8,     x8,     32
+addi    x10,    x10,    1
+bne     x10,    x9,     -136
--- projects/lab3/vsrc/mac/mac_unit.v
+++ projects/lab3/vsrc/mac/mac_unit.v
@@ -11,5 +11,56 @@ module mac_unit(
     output [`VREG_BUS]  mac_result_o
 );
 
+reg [`VREG_BUS] vreg_bias;
+reg [`VREG_BUS] vreg_result;
+reg [`VREG_BUS] output_temp;
+reg [`VREG_BUS] mul_acc;
+
+assign mac_result_o = output_temp ;
+
+always @(posedge clk) begin
+    if( rst == 1'b1) begin
+      vreg_bias <= 'h0 ;
+      vreg_result <= 'h0 ;
+    end
+    else begin
+      case(alu_opcode_i)
+          `ALU_OP_VMAC_LW : begin
+            vreg_bias <= mac_op_v1_i ;
+          end
+          `ALU_OP_VMAC_SW : begin
+            output_temp <= vreg_result;
+          end
+          `ALU_OP_VMAC_EN : begin
+                     mul_acc[31:0] <= mac_op_v1_i[31:0]    * mac_op_v2_i [31:0]    +
+                                      mac_op_v1_i[63:32]   * mac_op_v2_i [63:32]   +
+                                      mac_op_v1_i[95:64]   * mac_op_v2_i [95:64]   +
+                                      mac_op_v1_i[127:96]  * mac_op_v2_i [127:96]  +
+                                      mac_op_v1_i[159:128] * mac_op_v2_i [159:128] +
+                                      mac_op_v1_i[191:160] * mac_op_v2_i [191:160] +
+                                      mac_op_v1_i[223:192] * mac_op_v2_i [223:192] +
+                                      mac_op_v1_i[255:224] * mac_op_v2_i [255:224] ;
+            case(mac_sel_i)
+              3'b000: vreg_result[31:0]    <= mul_acc[31:0] + vreg_bias[31:0]    ;
+              3'b001: vreg_result[63:32]   <= mul_acc[31:0] + vreg_bias[63:32]   ;
+              3'b010: vreg_result[95:64]   <= mul_acc[31:0] + vreg_bias[95:64]   ;
+              3'b011: vreg_result[127:96]  <= mul_acc[31:0] + vreg_bias[127:96]  ;
+              3'b100: vreg_result[159:128] <= mul_acc[31:0] + vreg_bias[159:128] ;
+              3'b101: vreg_result[191:160] <= mul_acc[31:0] + vreg_bias[191:160] ;
+              3'b110: vreg_result[223:192] <= mul_acc[31:0] + vreg_bias[223:192] ;
+              3'b111: vreg_result[255:224] <= mul_acc[31:0] + vreg_bias[255:224] ;
+            endcase
+          end
+            default: begin
+              vreg_bias <= 'h0 ;
+              vreg_result <= 'h0 ;
+            end
+
+      endcase
+    end
+end
+
+
+
 
 endmodule
