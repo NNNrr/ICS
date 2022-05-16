1bd420bca9793b6636793e67c582e311786b6975
ICS-ShanghaiTech <tracer@ics>
2022-05-16 17:11:56 +0800

    an 2020231013

--- projects/lab3/vsrc/vector/vexe_stage.v
+++ projects/lab3/vsrc/vector/vexe_stage.v
@@ -16,10 +16,24 @@ always @(*) begin
     else begin
         case(alu_opcode_i)
         `ALU_OP_VADD: begin
-            vexe_result_o = operand_vs1_i + operand_vs2_i ;
+            vexe_result_o [31:0] = operand_vs1_i [31:0] + operand_vs2_i [31:0] ;
+            vexe_result_o [63:32] = operand_vs1_i [63:32] + operand_vs2_i [63:32] ;
+            vexe_result_o [95:64] = operand_vs1_i [95:64] + operand_vs2_i [95:64] ;
+            vexe_result_o [127:96] = operand_vs1_i [127:96] + operand_vs2_i [127:96] ;
+            vexe_result_o [159:128] = operand_vs1_i [159:128] + operand_vs2_i [159:128] ;
+            vexe_result_o [191:160] = operand_vs1_i [191:160] + operand_vs2_i [191:160] ;
+            vexe_result_o [223:192] = operand_vs1_i [223:192] + operand_vs2_i [223:192] ;
+            vexe_result_o [255:224] = operand_vs1_i [255:224] + operand_vs2_i [255:224] ;
         end
         `ALU_OP_VMUL: begin
-            vexe_result_o = (operand_vs1_i * operand_vs2_i) ;
+            vexe_result_o [31:0] = operand_vs1_i [31:0] * operand_vs2_i [31:0] ;
+            vexe_result_o [63:32] = operand_vs1_i [63:32] * operand_vs2_i [63:32] ;
+            vexe_result_o [95:64] = operand_vs1_i [95:64] * operand_vs2_i [95:64] ;
+            vexe_result_o [127:96] = operand_vs1_i [127:96] * operand_vs2_i [127:96] ;
+            vexe_result_o [159:128] = operand_vs1_i [159:128] * operand_vs2_i [159:128] ;
+            vexe_result_o [191:160] = operand_vs1_i [191:160] * operand_vs2_i [191:160] ;
+            vexe_result_o [223:192] = operand_vs1_i [223:192] * operand_vs2_i [223:192] ;
+            vexe_result_o [255:224] = operand_vs1_i [255:224] * operand_vs2_i [255:224] ;
         end
         default: begin
             vexe_result_o = 0;
