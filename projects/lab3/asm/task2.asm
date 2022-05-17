2437865842cd007c91712219a4602d37e72d7b48
ICS-ShanghaiTech <tracer@ics>
2022-05-17 10:18:46 +0800

    an 2020231013

--- projects/lab3/asm/task2.asm
+++ projects/lab3/asm/task2.asm
@@ -9,15 +9,16 @@ addi    x12,    zero,   0
 
 addi    x13,    x5,     0
 addi    x14,    x8,     4
-addi    x15,    x8,     16
+addi    x15,    x8,     32
 
 vle32.v vx2,    x13,    1
 vle32.v vx3,    x6,     1
 vmul.vv vx4,    vx2,    vx3,    0
 
 
-vse32.v vx4,    x15,     1
-;vle32.v vx1,    x8,     1
+vse32.v vx4,    x8,     1
+vle32.v vx1,    x14,    1
+vse32.v vx1,    x15,    1
 
 ;vse32.v vx1,    x14,    1
 ;vle32.v vx5,    x15,    1
