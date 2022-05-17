8163a34565e567b09929dbfcc93795c8d27773f3
ICS-ShanghaiTech <tracer@ics>
2022-05-17 14:19:23 +0800

    an 2020231013

--- projects/lab3/asm/task3.asm
+++ projects/lab3/asm/task3.asm
@@ -30,7 +30,7 @@ addi    x5,     x5,     32
 vle32.v vx2,    x5,     1
 vmac.en 7,      vx2,    vx3
 vmac.sw vx1
-vse32.v vx1,     x8,     1
+vse32.v vx1,     x8,    1
 addi    x5,     x1,     0   
 addi    x6,     x6,     32
 addi    x7,     x7,     32
