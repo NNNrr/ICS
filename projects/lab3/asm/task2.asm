6abe36e81357f6f4dbc814c7568576bbe0e446c0
ICS-ShanghaiTech <tracer@ics>
2022-05-17 09:53:24 +0800

    an 2020231013

--- projects/lab3/asm/task2.asm
+++ projects/lab3/asm/task2.asm
@@ -3,36 +3,26 @@ addi    x6,     x2,     0   ; addr_B = B_baseaddr
 addi    x7,     x3,     0   ; addr_C = C_baseaddr
 addi    x8,     x4,     0   ; addr_D = D_baseaddr
 addi    x9,     zero,   8
-addi    x13,    x8,     256
-addi    x16,    zero,   0
 addi    x10,    zero,   0
 addi    x11,    zero,   0
-addi    x12,    x5,     0    
-vle32.v vx3,    x6,     1           ; vx3 = mem[addr_B]
-vle32.v vx2,    x12,    1           ; vx2 = mem[addr_A]
-vmul.vv vx4,    vx3,    vx2,    0   ; vx4 = vx2 * vx3
-vse32.v vx4,    x13,    1           ; mem[addr_D] = vx1
-addi    x16,    zero,   0
-lw      x14,    0(x13)
-add     x15,    x15,    x14
-addi    x13,    x13,    4
-addi    x16,    x16,    1
-bne     x16,    x9,     -20
-sw      x15,    0(x8)
-addi    x8,     x8,     4
-addi    x15,    zero,   0
-addi    x12,    x12,    32
-addi    x11,    x11,    1
-bne     x11,    x9,     -60
-addi    x6,     x6,     32
-addi    x10,    x10,    1
-bne     x10,    x9,     -84
-addi    x8,     x4,     0   ; addr_D = D_baseaddr
-vle32.v vx5,    x8,     1
-vle32.v vx6,    x7,     1
-vadd.vv vx6,    vx6,    vx5,    0
-vse32.v vx6,    x8,     1    
-addi    x7,     x7,     32
-addi    x8,     x8,     32
-addi    x17,    x17,    1
-bne     x17,    x9,     -32
+addi    x12,    zero,   0
+
+addi    x13,    x5,     0
+addi    x14,    x8,     4
+addi    x15,    x8,     32
+
+vle32.v vx2,    x13,    1
+vle32.v vx3,    x6,     1
+vmul.vv vx4,    vx2,    vx3,    0
+
+
+vse32.v vx4,    x8,     1
+
+vse32.v vx4,    x14,    1
+vle32.v vx1,    x15,    1
+vadd.vv vx1,    vx1,    vx4,    0
+vse32.v vx1,    x8,     1
+
+addi    x14,    x14,    4
+addi    x12,    x12,    1
+bne     x12,    x9,     -28
